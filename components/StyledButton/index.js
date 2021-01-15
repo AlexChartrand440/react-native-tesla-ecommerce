dd87f1b8e6bf0cdff7a2a48017c2cb13a4d4a999
John Mwaura <johnmwaura08@gmail.com>
2021-01-15 13:06:18 -0600

    use props for content and onPress

--- components/StyledButton/index.js
+++ components/StyledButton/index.js
@@ -2,18 +2,18 @@ import React from "react";
 import { View, Text, Pressable } from "react-native";
 import styles from "./styles";
 const StyledButton = (props) => {
-  const type = props.type;
-  const backgroundColor = type === "primary" ? "black" : "white";
-  const textColor = type === "primary" ? "white" : "black";
+  const { type, content, onPress } = props;
+
+  const backgroundColor = type === "primary" ? "#171A20CC" : "#FFFFFFA6";
+  const textColor = type === "primary" ? "#FFFFFF" : "#171A20";
+
   return (
     <View style={styles.container}>
       <Pressable
         style={[styles.button, { backgroundColor: backgroundColor }]}
-        onPress={() => {
-          console.log("Hey There");
-        }}
+        onPress={() => onPress()}
       >
-        <Text style={[styles.text, { color: textColor }]}>Custom Order</Text>
+        <Text style={[styles.text, { color: textColor }]}>{content}</Text>
       </Pressable>
     </View>
   );
