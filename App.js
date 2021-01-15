1f6f4dafff84a2f958e76cd0ae6ab5cedb99c5fb
John Mwaura <johnmwaura08@gmail.com>
2021-01-15 13:39:34 -0600

    pass down props to car item component

--- App.js
+++ App.js
@@ -2,10 +2,16 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View, ImageBackground } from "react-native";
 import CarItem from "./components/carItem";
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <CarItem />
+      <CarItem
+        name={"Model X"}
+        tagline={"Order Online For"}
+        taglineCTA={"Touchless Delivery"}
+        image={require("./assets/images/ModelX.jpeg")}
+      />
       <StatusBar style="auto" />
     </View>
   );
