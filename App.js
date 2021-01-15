6bcf7d8216cdc851b8ed482712c768773a180113
John Mwaura <johnmwaura08@gmail.com>
2021-01-15 13:59:58 -0600

    import carlist to render list of cars

--- App.js
+++ App.js
@@ -1,17 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, ImageBackground } from "react-native";
-import CarItem from "./components/carItem";
+import { StyleSheet, View } from "react-native";
+import CarList from "./components/CarList";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <CarItem
-        name={"Model X"}
-        tagline={"Order Online For"}
-        taglineCTA={"Touchless Delivery"}
-        image={require("./assets/images/ModelX.jpeg")}
-      />
+      <CarList />
       <StatusBar style="auto" />
     </View>
   );
